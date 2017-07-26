06adc5f6e4a52cb5902a19d9cc41fbd603cb743f
Jared Moore <swiftfoottim@gmail.com>
2017-07-26 09:31:29 -0400

    Modified the gruntfile to use variables making it so you only have to change one line.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,19 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('./package.json'),
     assignments: grunt.file.readJSON('src/data/assignments.json'),
+    projects: grunt.file.readJSON('src/data/projects.json'),
+    
+    cfg: {
+      base_web_addr: 'http://www.cis.gvsu.edu/~moorejar',
+      dest: '/Users/moorejar/Dropbox/Faculty_Position/Course_Materials/Course_Websites/',
+      dist: '/Default_Template/'
+    },
 
     assemble: {
       options: {
-        assets: './static',
+        assets: '.<%= cfg.dist %>static',
+        assets_dir: 'static',
+        course_link: '<%= cfg.base_web_addr %><%= cfg.dist %>',
         data: './src/data/*.yml',
         layout: 'page.hbs',
         layoutdir: './src/build/layouts/',
@@ -19,18 +28,27 @@ module.exports = function (grunt) {
           pages: '<%= assignments.assigns %>'
         },
         files: {
-          'dist/': ['!*']
+          'dist<%= cfg.dist %>': ['!*']
         }
       },
-      posts: {
+      assigned_project: {
+        options: {
+          layout: "project.hbs",
+          pages: '<%= projects.project %>'
+        },
+        files: {
+          'dist<%= cfg.dist %>': ['!*']
+        }
+      },
+      pages: {
         files: [{
           cwd: './src/content/',
-          dest: './dist/',
+          dest: './dist<%= cfg.dist %>',
           expand: true,
           src: ['**/*.hbs', '!_pages/**/*.hbs']
         }, {
           cwd: './src/content/_pages/',
-          dest: './dist/',
+          dest: './dist<%= cfg.dist %>',
           expand: true,
           src: '**/*.hbs'
         }]
@@ -53,9 +71,17 @@ module.exports = function (grunt) {
           src: [
             '**/*'
           ],
-          dest: './dist/static/',
+          dest: './dist<%= cfg.dist %>static/',
           /*filter: 'isFile',*/
-          cwd: 'src/static/'
+          cwd: './src/static/'
+        }]
+      },
+      'migration': {
+        files: [{
+          cwd: './dist',  // set working folder / root to copy
+          src: ['**/*'],           // copy all files and subfolders
+          dest: '<%= cfg.dest %>',    // destination folder
+          expand: true           // required when using cwd
         }]
       }
     }
